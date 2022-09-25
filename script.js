fc2d1c3b1a583408739824a26b22d2517a870c48
MartinPanelo <panelomartinn@hotmail.com>
2022-09-24 22:42:08 -0300

    implementacion de borrar elemento de la lista (funciona)

--- script.js
+++ script.js
@@ -5,54 +5,42 @@ let dic = document.getElementById("d");
 let list = document.getElementById("myList");
 /* --------------------------------- */
 
-let palabras = ['HOLA', 'CHAU', 'MARMOTA'];
-var mi_array = ['cero', 'uno', 'dos', 'tres', 'cuatro'];
+let palabras = ["HOLA", "CHAU", "MARMOTA"];
+var mi_array = ["cero", "uno", "dos", "tres", "cuatro"];
 init();
 
-
-function init(){
-
-   menu.style.display = 'visibility'; // 'none'
-    d.style.display = 'none'; // 'none'
+function init() {
+  menu.style.display = "visibility"; // 'none'
+  d.style.display = "none"; // 'none'
 }
 
-function borrar(b){
+function borrar(b) {
+  palabras.splice(b, 1);
+  //  list.removeChild(list.childNodes[b]);
 
-    palabras.splice(b,b+1);
-   
-
-   
-   list.removeChild(list.childNodes[b]);
-    
+  glosario();
 }
 
-function glosario(){
-    menu.style.display = 'none'; // 'none'
-    d.style.display = 'flex'; // 'none'
- 
+function glosario() {
+  menu.style.display = "none"; // 'none'
+  d.style.display = "flex"; // 'none'
+  list.innerHTML = "";
 
-    palabras.forEach((item,index) => {
+  palabras.forEach((item, index) => {
+    let b = document.createElement("button");
+    let ul = document.createElement("ul");
 
-        let b = document.createElement("button");
-      
-        b.innerHTML = "Borrar";
-       
-        b.addEventListener("click", function () {
-            borrar(index);
-          });
+    b.innerHTML = "Borrar";
 
-        let ul = document.createElement("ul");
-        
-        ul.innerText = item;
-        list.appendChild(ul);
-        ul.appendChild(b);
-      });
-    
+    b.addEventListener("click", function () {
+      borrar(index);
+    });
+
+    ul.innerText = item;
+
+    list.appendChild(ul);
+    ul.appendChild(b);
+  });
 }
-
-
-
-
-
 
 button2.onclick = glosario;
